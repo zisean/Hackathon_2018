ecba11a2347ed123f8bf6590de736e2e2025e522
{zisean} <{zxchow@gmail.com}>
2018-04-15 13:18:44 +0800

    update code

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-//var http = require('http').Server(app);
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb'}));
@@ -22,9 +21,9 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
-// http.listen(3000, function(){
-// 	console.log('listen on *:3000');
-// });
+app.get('/register', function(req, res){
+	res.sendFile(__dirname + '/register.html');
+});
 
 router.get('/', function(req, res){
 
@@ -32,7 +31,8 @@ router.get('/', function(req, res){
 });
 
 app.post('/getData', function(req, res){
-	var mysql      = require('mysql');
+
+var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -57,8 +57,7 @@ connection.query('SELECT * FROM users', function (error, results, fields) {
 });
 
 connection.end();
-	// var obj = {};
-	// console.log('body: ' + JSON.stringify(req.body));
+
 });
 
 // Post from client
@@ -121,54 +120,11 @@ router.post('/checkin', function(req, res){
 		res.send('success');
   	}
 
-  	req.body.type = 'Clock-in';
+  	req.body.type = 'Clock-in'; 
 	
 	socket.emit('checkinObj',req.body);
 	
 });
-
-// // Testing
-// router.get('/checkin', function(req, res){
-
-// 	var socket = io;
-
-// 	var checkin = { status: 'SUCCESS',
-//   					data: { username: 'Edward' },
-//   					timestamp: 1523701749091 };
-
-//   //	io.on('connection', (socket) => {
-//   		socket.emit('checkinObj',checkin);
-// //});
-  
-
-// 	console.log(checkin);
-
-// 	var mysql = require('mysql');
-
-// 		var con = mysql.createConnection({
-// 			host: "localhost",
-// 			user: "root",
-// 			password: "",
-// 			database: "facekathon"
-// 		});
-
-// 		con.connect(function(err){
-
-// 		if(err) throw err;
-// 			console.log("Connected")
-// 			var sql = "INSERT INTO users (username, status, timestamp, image_name) VALUES ('" 
-// 			+ checkin.data.username + "','" + checkin.status + "','" + checkin.timestamp + "','" + "')";
-// 			con.query(sql, function(err, result){
-// 				if (err) throw err;
-// 				console.log("1 record");
-// 			});
-// 		});
-
-// 	res.send('success');
-
-
-	
-// });
 
 router.post('/register', function(req, res){
 	var mysql = require('mysql');
@@ -179,6 +135,8 @@ router.post('/register', function(req, res){
 		password: "",
 		database: "facekathon"
 	});
+
+	console.log(req.body);
 	res.send('register');
 });
 
@@ -250,6 +208,5 @@ router.post('/checkout', function(req, res){
 
 app.use('/api', router);
 
-//app.listen(port);
 console.log('Start listening on port' + port);
 
